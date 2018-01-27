b7a99a0e3cf6d953d1a89a30853af4bd5ac6528b
utkarsh867 <utkarsh867@gmail.com>
2018-01-28 00:19:55 +0800

    mongodb test

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -14,8 +13,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+//MongoDB database
+var mongodb = require('mongodb');
+var url = "mongodb://127.0.0.1:27017";
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    mongodb.MongoClient.connect(url, function (err,db) {
+        if(err){
+            console.log(err);
+        }
+        else{
+            try{
+                console.log(JSON.stringify(db));
+                res.send("Connected to database\n\r");
+            }catch (exception){
+                console.log(exception);
+            }
+        }
+    });
 });
 
 module.exports = router;
