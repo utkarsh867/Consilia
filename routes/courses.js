a17c686f1ffc78d49dd9b1bdd5b23cf82450f7d1
utkarsh867 <utkarsh867@gmail.com>
2018-01-28 08:10:01 +0800

    courses route

--- routes/courses.js
+++ routes/courses.js
@@ -4,27 +4,27 @@ var router = express.Router();
 //post event of type
 router.post("/course_suggestion",function(req,res){
     var db =req.db;
-    var selected = req.body.data
-    var passed = []
-    var enrolled = []
-    var course_list = {}
-    var callbacks = 0
-    var total = 0
+    var selected = req.body.data;
+    var passed = [];
+    var enrolled = [];
+    var course_list = {};
+    var callbacks = 0;
+    var total = 0;
     Object.keys(selected).forEach(function(year){
         Object.keys(selected[year]).forEach(function(sem){
-            var enrolled = selected[year][sem]
+            var enrolled = selected[year][sem];
             var add=true;
-            var final_docs=[]
-            total += 1
-            callbacks += 1
+            var final_docs=[];
+            total += 1;
+            callbacks += 1;
             db.get("courses").find({"code":{$ne:{$all:passed}}},{"code":1,"name":1},function(err,doc){
                     if(err)
                         throw err;
                     for(var i in doc)
                     {
                         for ( var j in doc[i].criteria){
-                            var strings_p=doc[i].criteria[j].passed
-                            var strings_e=doc[i].criteria[j].enrolled
+                            var strings_p=doc[i].criteria[j].passed;
+                            var strings_e=doc[i].criteria[j].enrolled;
                             if(eachel(passed,strings_p) ||(!strings_e&&eachel(enrolled,strings_e)))
                             {
                                 add=true;}
@@ -44,42 +44,41 @@ router.post("/course_suggestion",function(req,res){
                         course_list[year][sem] = final_docs;
                     }
                     callbacks -= 1;
-                    if (total == 8 && callbacks == 0){
+                    if (total === 8 && callbacks === 0){
                         res.send(course_list);
                     }
-            })
+            });
             passed.concat(enrolled)
         });
     });
-})
+});
+
 router.get("/course_period",function (req,res){
     var db=req.db;
     //To clarify about the year param
     var query={};
-    var year=[1,2,3,4]
+    var year=[1,2,3,4];
     if (req.query.yr)
-        query.year=year.splice(req.query.yr-1)
+        query.year=year.splice(req.query.yr-1);
 
     if(req.query.sem)
         query.semester=parseInt(req.query.sem);
-    console.log(query)
+    console.log(query);
     db.get("courses").find(query,{},function(err,doc) {
         if(err)
-            throw err
+            throw err;
+
         res.send(doc);
 
-    })
+    });
+});
 
-
-})
-
-module.exports=router
 function eachel(set,subset) {
     var h = true;
     for (var i in subset) {
         h=false;
         for (var j in set) {
-            if (set[j] == subset[i]) {
+            if (set[j] === subset[i]) {
                 h = true;
                 break;
             }
@@ -87,11 +86,14 @@ function eachel(set,subset) {
     }
     return h;
 }
+
 function isSubsetOf(set, subset) {
     for (var i = 0; i < set.length; i++) {
-        if (subset.indexOf(set[i]) == -1) {
+        if (subset.indexOf(set[i]) === -1) {
             return false;
         }
     }
     return true;
 }
+
+module.exports = router;
